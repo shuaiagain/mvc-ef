eddee746a2906ee35fdeeac125a922fedd96655f
shuaiagain <764258872@qq.com>
2018-07-30 18:02:37 +0800

    1.添加html辅助类的使用

---
+++ MvcEFDemo/MvcEFDemo/Controllers/MVCDemoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcEFDemo.Controllers
+{
+    public class MVCDemoController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SharedDateDemo()
+        {
+            ViewBag.DateTime = DateTime.Now;
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult PartialViewDate()
+        {
+            ViewBag.DateTime = DateTime.Now.AddMinutes(10);
+            return View("PartialPageDateTime");
+        }
+    }
+}
